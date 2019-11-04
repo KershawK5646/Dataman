4e80d4596972b9ea2051f1b3299822d2233208f0
Kyler Kershaw <KershawK5646@student.faytechcc.edu>
2019-11-04 12:28:43 -0500

    Working on popQuiz.
    Added rough backbone of pop quiz module

--- sprint2_dataman/popQuiz.py
+++ sprint2_dataman/popQuiz.py
@@ -5,94 +5,52 @@ PopQuiz:
     for the user to practice with. This will function similarly to 
     flash-cards.
 """
-
-import json
+# Imports
 import dataManUtil as dmUtil
+import random
 
+# Main loop
 def popQuiz():
+    # Bool to control game state
+    gameLoop = True
     print("DATAMAN Pop Quiz")
-    print("\n1. Create \n2. Study \n3. Exit")
-    choice = dmUtil.getUserInput()
-
-    if choice == 1:
-        createCards()
-    elif choice == 2:
-        useCards()
-    elif choice == 3:
-        print("Returning...")
-    else:
-        print("Invalid option. Try again.")
-        popQuiz()
-
-def createCards():
-    # create array of user input math problems
-    # for user to solve
-    print("Let's make some flashcards.")
-    print("How many cards?")
-    count = dmUtil.getUserInput()
-    # number of flashcards is set given by user
-    print("Type the problem, hit the spacebar, type the answer, and then hit enter.")
-    cards = dict(input().split() for _ in range(count))
-    # this creates a dictionary using user input
-    # user can input problem, then a space, and then the correct answer
-    # example: '2+2' ``space key`` '4'
-    # this will be put into dictionary with '2+2' as the key
-    # and '4' will be the value
-    json.dump(cards, open('cards.csv','w'))
-    print(cards) # for now, this verifies the user made the cards correctly
-    popQuiz()
-
-def useCards():
-    # user is given previously input math problems
-    # and their score
-    cards = json.load(open('cards.csv','r'))
-    correct = 0
-    incorrect = 0
-    total = 0
-    print("Ready? Go!")
+    while gameLoop = True:
+        # Generatr randoms needed for numbers and operator
+        number1 = getRandomNumber()
+        number2 = getRandomNumber()
+        operator = getRandomOperator()
+        
+        # Propose the question
+        #questionPropt(number1 + " " + operator + " " number2 + "= :")
+        
+        #print(questionPrompt)
+        
+        # Go again question
+        gameLoop = again()
     
-    for key in cards:
-        print(key)
-        print("Answer: ")
-        guess = dmUtil.getUserInput()
+# Generate and return a random number
+def getRandomNumber():
+    randomNumber = randint(0,10)
+    print(randomNumber)
+    return randomNumber
 
-        if str(guess) == cards[key]:
-            print("Correct!")
-            correct += 1
-            total += 1
-        else:
-            print("Incorrect!")
-            incorrect += 1
-            total += 1
-
-    score = (correct / total) * 100
-    print("Correct: ",correct,"Incorrect: ",incorrect)
-
-    if score > 90:
-        print("Great job!")
-        print("Score:",score,'%')
-    elif score > 80:
-        print("Good job!")
-        print("Score:",score,'%')
-    elif score > 60:
-        print("Keep it up!")
-        print("Score:",score,'%')
-    else:
-        print("Better luck next time!")
-        print("Score:",score,'%')
-
-    again()
-
+# Use a random number to get a random operator
+def getRandomOperator():
+    operatorSelection = randint(0,2)
+    print(operatorSelector)
+    
+# Go again function
 def again():
     print("Would you like to study the same cards again?")
     print("\n1. Yes \n2. No")
     choice = dmUtil.getUserInput()
     
+    # Repeat selection
     if choice == 1:
-        useCards()
+        return True
+    # Quit selection
     elif choice == 2:
-        print("Returning...")
-        popQuiz()
+        return False
 
 if __name__ == '__main__':
     popQuiz()
